d9821fedc0fecfd4039cd23d88659be4c307ee22
Diego Rodriguez <dierodz91@gmail.com>
2021-10-20 09:59:11 -0300

    create component searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React from "react";
 
-export default function SearchBar(props) {
+export default function SearchBar({ onSearch }) {
   // acá va tu código
-  return <div>Search Bar Component</div>
-};
+  function handleOnSearch() {
+    if (typeof onSearch === "function") {
+      const input = document.getElementById("search-bar-input");
+      onSearch(input.value);
+    }
+  }
+
+  return (
+    <div>
+      <input id="search-bar-input" />
+      <button onClick={handleOnSearch}>Agregar</button>
+    </div>
+  );
+}
